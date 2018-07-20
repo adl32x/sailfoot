98e6a3caa5292eaf4687d57c3eea8be827f20625
adl32x <andreas.lindroos@gmail.com>
2018-07-20 08:54:31 +0300

    Cont.

--- log/console.go
+++ log/console.go
@@ -3,19 +3,27 @@ package log
 import (
 	"fmt"
 
-	aurora "github.com/logrusorgru/aurora"
+	"github.com/fatih/color"
 )
 
 var colors = true
+var red = color.New(color.FgRed).SprintFunc()
+var green = color.New(color.FgGreen).SprintFunc()
 
 func Log(str string) {
-	fmt.Println("%s "+str, aurora.Green("."))
+	fmt.Println("%s "+str, green("."))
+}
+
+func Debug(args ...interface{}) {
+	for i := 0; i < len(args); i++ {
+		fmt.Print((args[i]))
+	}
+	fmt.Print("\n")
 }
 
 func Error(args ...interface{}) {
 	for i := 0; i < len(args); i++ {
-		fmt.Print(aurora.Red(args[i]))
+		fmt.Print(red(args[i]))
 	}
 	fmt.Print("\n")
-
 }
